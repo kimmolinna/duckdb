3ff645ea7a55e6d5d1a4f349d82433189cd791b4
Mark Raasveldt <mark.raasveldt@gmail.com>
2023-01-16 13:22:23 +0100

    Correctly read stats for time/timestamp with time zone

--- extension/parquet/parquet_statistics.cpp
+++ extension/parquet/parquet_statistics.cpp
@@ -158,31 +158,11 @@ Value ParquetStatisticsUtils::ConvertValue(const LogicalType &type,
 		return Value::DATE(date_t(Load<int32_t>((data_ptr_t)stats.c_str())));
 	case LogicalTypeId::TIME:
 	case LogicalTypeId::TIME_TZ: {
-		int64_t val;
-		if (stats.size() == sizeof(int32_t)) {
-			val = Load<int32_t>((data_ptr_t)stats.c_str());
-		} else if (stats.size() == sizeof(int64_t)) {
-			val = Load<int64_t>((data_ptr_t)stats.c_str());
-		} else {
+		if (stats.size() != sizeof(int64_t)) {
 			throw InternalException("Incorrect stats size for type TIME");
 		}
-		if (schema_ele.__isset.logicalType && schema_ele.logicalType.__isset.TIME) {
-			// logical type
-			if (schema_ele.logicalType.TIME.unit.__isset.MILLIS) {
-				return Value::TIME(Time::FromTimeMs(val));
-			} else if (schema_ele.logicalType.TIME.unit.__isset.NANOS) {
-				return Value::TIME(Time::FromTimeNs(val));
-			} else if (schema_ele.logicalType.TIME.unit.__isset.MICROS) {
-				return Value::TIME(dtime_t(val));
-			} else {
-				throw InternalException("Time logicalType is set but unit is not defined");
-			}
-		}
-		if (schema_ele.converted_type == duckdb_parquet::format::ConvertedType::TIME_MILLIS) {
-			return Value::TIME(Time::FromTimeMs(val));
-		} else {
-			return Value::TIME(dtime_t(val));
-		}
+		auto time = dtime_t(Load<int64_t>((data_ptr_t)stats.c_str()));
+		return Value::TIME(time);
 	}
 	case LogicalTypeId::TIMESTAMP:
 	case LogicalTypeId::TIMESTAMP_TZ: {
